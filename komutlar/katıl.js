b0b5004affd736a027c1952f4479b8934c7b34c3
s0met <64663719+s0met@users.noreply.github.com>
2020-10-12 09:08:59 +0300

    Create katıl.js

---
+++ komutlar/katıl.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+module.exports.run = async (client, msg, args) => {
+
+    let kayıt = msg.guild.member(msg.mentions.users.first()) || msg.guild.member(args[0]);
+    if (!kayıt) return msg.channel.send({
+        embed: {
+            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+            description: ("📥 Kendini etiketlemelisin!")
+        }
+    })
+
+    let role = msg.guild.roles.find(r => r.name === "Üye");
+    if (!role) {
+        try {
+            role = await msg.guild.Role({
+            });
+
+            msg.guild.channels.forEach(async (channel, id) => {
+                await channel.overwritePermissions(role, {
+                    
+                });
+            });
+        } catch (e) {
+            console.log(e.stack);
+        }
+
+    }
+
+    if (kayıt.roles.has(role.id)) return msg.channel.send({
+        embed: {
+            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+            description: (" 💡 Önceden Katılmışsınız Pintilik yapma ")
+        }
+    });
+
+    await kayıt.addRole(role);
+    msg.channel.send({
+        embed: {
+            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+            description: ("🔑 Çekilişe Katıldınız")
+        }
+    })
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'katıl',
+    description: 'Çekilişe Katılmış Olursunuz',
+    usage: 'katıl'
+};
