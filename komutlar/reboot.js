5136ec27f926948598b0490550cfb58c65d474a3
s0met <64663719+s0met@users.noreply.github.com>
2020-10-12 09:09:41 +0300

    Create reboot.js

---
+++ komutlar/reboot.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const ayarlar = require("../ayarlar.json")
+
+module.exports.run = async (bot, message, args) => { 
+   
+  var embed2 = new Discord.RichEmbed()   
+      .setTitle('Merhaba, ' + message.member.user.username)
+      .setDescription('sadece sahibim bu komutu  kullanabilir! Yani Pudochu `R#0001 ')
+      .setColor('RED') 
+  
+
+  if(message.author.id !== "722771274375364659") return message.channel.sendEmbed(embed2)
+     
+  //Cortex botun reboot sistemi hadi h.o
+  var embed = new Discord.RichEmbed()   
+      .setTitle('**Merhaba Sahibim samettexe,**')
+      .setDescription('Beni yeniden başlatmak  istediğine eminsen aşağıdaki **TİK** işaretine, bir kere dokunur musun?')
+      .setColor('RANDOM')
+message.channel.send(embed).then(async function (sentEmbed) {
+			const emojiArray = ["✅"];
+			const filter = (reaction, user) => emojiArray.includes(reaction.emoji.name) && user.id === message.author.id;
+			await sentEmbed.react(emojiArray[0]).catch(function () { });
+			var reactions = sentEmbed.createReactionCollector(filter, {
+				time: 30000
+			});
+reactions.on("end", () => message.delete().then(mr => sentEmbed.delete()).then(m => message.delete()).then(m2 => message.author.send("Yeniden Başlatma İşlemi iptal ettim! "))) 
+    reactions.on("collect", async function (reaction) {
+				if (reaction.emoji.name === "✅") {
+  try {
+    message.delete().then(mr => sentEmbed.delete()).then(wb => { 
+ console.log(`BOT: Bot yeniden başlatılıyor...`);
+    process.exit(0);
+    })
+  } catch (err) {
+  // Pudochu
+    message.channel.send(`**Hata:** \n\`\`\`js\n${err}\n\`\`\``);
+  
+};
+
+        }
+    })
+})
+
+};
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['reboot', 'Reboot', 'yenidenbaşlat', 'yenile', 'rebot', 'rebooot', 'reboott', 'treboot'],
+  permLevel: 0
+};
+
+module.exports.help = {
+  name: 'reboot',
+  description: 'Sistemi yeniden başlatır',
+  usage: 'reboot'
+};
