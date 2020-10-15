d5a7e09ef2d3dea52d85cd752ca14d27804af0a3
samettexe <64663719+s0met@users.noreply.github.com>
2020-10-15 14:35:24 +0300

    Update canvas-kapat.js

--- komutlar/canvas-kapat.js
+++ komutlar/canvas-kapat.js
@@ -1,34 +1,41 @@
 const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json");
 const db = require("quick.db");
- 
+const ayarlar = require("../ayarlar.json");
 exports.run = async (client, message, args) => {
   if (!message.member.hasPermission("MANAGE_GUILD"))
     return message.reply(
-      `Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`
+      `Bu komutu kullanabilmek için **Sunucuyu Yönet** iznine sahip olmalısın!`
     );
+  //'~'Resađ Seferov✨#0809
+  let prefix = ayarlar.prefix;
  
-  let channel = message.mentions.channels.first();
-  if (!channel) {
-    return message.reply("Bir kanal etiketleyin");
+  if (db.has(`gçkanal_${message.guild.id}` === false)) {
+    const embed = new Discord.RichEmbed()
+      .setDescription(`Giriş çıkışı Ayarlamadığın İçin Sıfırlayamazsın!`)
+      .setColor("RED")
+      .setTimestamp(`Ayarlamak İçin ${prefix}giriş-çıkış-ayarla #kanal`);
+    message.channel.send({ embed });
+    return;
   }
-  db.set(`gçkanal_${message.guild.id}`, channel.id);
-  //var i = db.set(`capsE_${message.guild.id}`, "acik")
+  db.delete(`gçkanal_${message.guild.id}`);
  
-  message.channel.send(
-    `:white_check_mark: | ** Resimli Hoşgeldin - Güle Güle kanalı ${channel} Olarak Ayarlandı.** `
-  );
+  const embed = new Discord.RichEmbed()
+    .setDescription(`Giriş Çıkış Başarıyla Sıfırlandı`)
+    .setColor("RANDOM")
+    .setTimestamp();
+  message.channel.send({ embed });
+  return;
 };
  
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["gç-ayarla"],
+  aliases: [],
   permLevel: 0
 };
  
 exports.help = {
-  name: "giriş-çıkış-ayarla",
-  description: "Giriş Çıkış Kanalını Ayarlar.",
-  usage: "gç-ayarla <#kanal>"
+  name: "hgbb-kapat",
+  description: "Giriş çıkışı kapatır",
+  usage: "hgbbkapat"
 };
